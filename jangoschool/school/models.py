4a088aa07e278f1356966589e13fdefef5085c67
R-comp <deadnullgamer@gmail.com>
2021-11-12 20:55:15 +0700

    First update

--- jangoschool/school/models.py
+++ jangoschool/school/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Score(models.Model):
+
+    allsubject=(('math','คณิตศาสตร์'),
+                ('science','วิทยาศาสตร์'),
+                ('art','ศิลปะ'),
+                ('eng','ภาษาอังกฤษ'))
+
+    subject = models.CharField(max_length=50,choices=allsubject,default='NULL')
+    std_name = models.CharField(max_length=100)
+    score=models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.std_name
