ee1c5b4f036ae2d952d3002321a63edde377e646
R-comp <deadnullgamer@gmail.com>
2021-11-16 19:28:04 +0700

    2.3

--- jangoschool/school/urls.py
+++ jangoschool/school/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('',home,name='home'),
-    path('about/',about,name='about'),
-    path('contact/',contact,name='contact'),
     path('score/',showscore,name='showscore'),
     path('register/',register,name='register')
 ]
--- jangoschool/school/views.py
+++ jangoschool/school/views.py
@@ -5,12 +5,6 @@ from django.contrib.auth.models import User
 def home(request):
     return render(request,'home.html')
 
-def about(request):
-    return render(request,'about.html')
-
-def contact(request):
-    return render(request,'contact.html')
-
 def showscore(request):
     score = Score.objects.all()
     context= {'score':score}
