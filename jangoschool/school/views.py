e6e6e4ac2faf852c0bc3084d827aaf94d6e3dbe2
R-comp <ramit.cop@gmail.com>
2021-11-26 12:17:56 +0700

    2.6 -- alert in register form

--- jangoschool/school/views.py
+++ jangoschool/school/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .models import *
 from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 def home(request):
     return render(request,'home.html')
@@ -19,14 +20,19 @@ def register(request):
         repassword = data.get('repassword')
         if password==repassword:
             if User.objects.filter(username=user_name).exists():
+                messages.info(request,"Username already exists")
                 return redirect('register')
             elif User.objects.filter(email=email).exists():
+                messages.info(request,"Email already exists")
                 return redirect('register')    
             else:
                 newuser=User()
                 newuser.username=user_name
                 newuser.email=email
                 newuser.set_password(password)
-                newuser.save()   
+                newuser.save()
+        else:
+            messages.info(request,"Password mismatch")
+            return redirect('register')
         return redirect('home')
     return render(request,'register.html')
